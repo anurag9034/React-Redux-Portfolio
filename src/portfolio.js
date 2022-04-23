3bc35a946dd0a2191c41ceec0eabac5a5109dbff
anurag9034 <kamanurag5@gmail.com>
2022-04-22 19:02:01 -0700

    Updated LDN

--- src/portfolio.js
+++ src/portfolio.js
@@ -38,7 +38,7 @@ const socialMediaLinks = [
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/anurag082/",
+    link: "https://www.linkedin.com/",
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
