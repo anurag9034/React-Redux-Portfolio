0b228c80b289bdb33f2d75e42aa72d01cfde6f36
anurag9034 <kamanurag5@gmail.com>
2022-04-23 10:41:06 -0700

    updated blogs

--- src/portfolio.js
+++ src/portfolio.js
@@ -431,12 +431,18 @@ const contactPageData = {
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development",
     profile_image_path: "profile_photo.png",
   },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    link: "ursanurag.com",
+    avatar_image_path: "blogs_image.svg",
+  },
   addressSection: {
     title: "Address",
     subtitle: "290 Bremner Blvd, Toronto, ON M5V 3L9",
     avatar_image_path: "address_image.svg",
-    location_map_link:
-      "https://www.google.com/maps/place/1260+E+University+Dr,+Tempe,+AZ+85281/@33.4225344,-111.9202503,17z/data=!3m1!4b1!4m5!3m4!1s0x872b08e4e45000ad:0xa287a193d6cb2d08!8m2!3d33.4225344!4d-111.9180616",
+    location_map_link: "https://goo.gl/maps/BMKVscANA9wNaqXf7",
   },
   phoneSection: {
     title: "Phone Number",
